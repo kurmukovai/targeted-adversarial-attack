1246d25658074119df22b8d4ce245fdb4f49f341
kurmukovai <kurmukovai@gmail.com>
2024-11-27 20:27:52 +0300

    add pgd attack

---
+++ targeted_adversarial.py
@@ -0,0 +1,13 @@
+import torch
+# adopted from https://adversarial-ml-tutorial.org/adversarial_examples/
+
+def pgd_linf_targ(model, X, y, epsilon, alpha, num_iter, y_targ):
+    """ Construct targeted adversarial examples on the examples X"""
+    delta = torch.zeros_like(X, requires_grad=True)
+    for t in range(num_iter):
+        yp = model(X + delta)
+        loss = (yp[:,y_targ] - yp.gather(1,y[:,None])[:,0]).sum()
+        loss.backward()
+        delta.data = (delta + alpha*delta.grad.detach().sign()).clamp(-epsilon,epsilon)
+        delta.grad.zero_()
+    return delta.detach()
