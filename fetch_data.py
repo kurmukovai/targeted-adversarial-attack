fc53ecbfe4d70fd480e62dd71639474a7006df5e
kurmukovai <kurmukovai@gmail.com>
2024-11-29 02:04:18 +0300

    fix tabulation

--- fetch_data.py
+++ fetch_data.py
@@ -1,13 +1,13 @@
 import json
 
 def match_labels_timm():
-  labels = dict()
-  with open("LOC_synset_mapping.txt", 'r') as f:
-    for line in f:
-      if line.strip():
-        key, value = line[:9].strip(), line[9:].strip()
-        labels[key] = value
-  return labels
+	labels = dict()
+	with open("LOC_synset_mapping.txt", 'r') as f:
+		for line in f:
+			if line.strip():
+				key, value = line[:9].strip(), line[9:].strip()
+				labels[key] = value
+	return labels
 
 if __name__=="__main__":
 	labels_timm_trained_model = match_labels_timm()
