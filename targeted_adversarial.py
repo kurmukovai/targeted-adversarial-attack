cef45120912a618adbc8bbddea52ba778dc06ccf
kurmukovai <kurmukovai@gmail.com>
2024-11-29 02:09:55 +0300

    update adv for gpu

--- targeted_adversarial.py
+++ targeted_adversarial.py
@@ -1,13 +1,32 @@
 import torch
+from tqdm import tqdm
 # adopted from https://adversarial-ml-tutorial.org/adversarial_examples/
+# TODO average over several training samples
 
 def pgd_linf_targ(model, X, y, epsilon, alpha, num_iter, y_targ):
     """ Construct targeted adversarial examples on the examples X"""
-    delta = torch.zeros_like(X, requires_grad=True)
-    for t in range(num_iter):
-        yp = model(X + delta)
-        loss = (yp[:,y_targ] - yp.gather(1,y[:,None])[:,0]).sum()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if model.training:
+        model.eval()
+
+    model = model.to(device)
+    delta = torch.zeros_like(X).detach().to(device)
+    delta.requires_grad_()
+
+    X = X.to(device)
+    y = y.to(device)
+    y_targ = y_targ.to(device)
+
+    for t in tqdm(range(num_iter)):
+        logits = model(X + delta)
+        
+        loss = (logits[:, y_targ] - logits.gather(1, y[:, None])[:,0]).sum()
         loss.backward()
-        delta.data = (delta + alpha*delta.grad.detach().sign()).clamp(-epsilon,epsilon)
-        delta.grad.zero_()
+
+        with torch.no_grad():
+            delta.data = (delta + alpha*delta.grad.sign()).clamp(-epsilon, epsilon)
+
+        if delta.grad is not None:
+            delta.grad.zero_()
+
     return delta.detach()
