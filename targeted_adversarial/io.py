04036efb453290d0cf0b47d7a38e0bb694365019
kurmukovai <kurmukovai@gmail.com>
2024-11-29 05:17:46 +0300

    save tensor

--- targeted_adversarial/io.py
+++ targeted_adversarial/io.py
@@ -1,4 +1,3 @@
-
 import requests
 import io
 import json
@@ -7,8 +6,9 @@ import torchvision
 import numpy as np
 from PIL import Image
 from pathlib import Path
-
-
+from typing import Dict
+from einops import rearrange
+from torchvision.utils import save_image
 
 def load_imagenet_classes() -> Dict[str, str]:
 	"""Load ImageNet synset IDs and their descriptions."""
@@ -32,10 +32,11 @@ def load_imagenet_sample_names() -> Dict[str, str]:
 def retrieve_imagenet_image(synset_class: str):
 	"""Downloads a single image from the ImageNet from `synset_class`."""
 	file_names = load_imagenet_sample_names() 
-	image_url = "https://raw.githubusercontent.com/EliSchwartz/imagenet-sample-images/refs/heads/master/{file_names[synset_class]}.JPEG"
+	image_url = f"https://raw.githubusercontent.com/EliSchwartz/imagenet-sample-images/refs/heads/master/{file_names[synset_class]}.JPEG"
+	print(image_url)
 	img_data = requests.get(image_url).content
 	img_pil = Image.open(io.BytesIO(img_data))
-	return np.array(img_pil)
+	return rearrange(torch.tensor((np.array(img_pil) / 255).astype(np.float32)), "h w c -> c h w")
 
 
 def normalize8(I):
@@ -50,3 +51,16 @@ def normalize8(I):
 
 def read_image(file: str):
 	return torchvision.io.read_image(file) / 255
+
+
+
+def save_tensor_as_jpeg(tensor, path):
+    # Ensure tensor is on CPU and detached from gradients
+    tensor = tensor.cpu().detach()
+    
+    # If tensor is in [H, W, C] format, convert to [C, H, W]
+    if tensor.dim() == 3 and tensor.shape[-1] == 3:
+        tensor = tensor.permute(2, 0, 1)
+
+    # Save image
+    save_image(tensor, path, format='JPEG')
